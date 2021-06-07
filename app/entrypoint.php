4b0d3592beeb6918c0a1fd649fd46d9c9c7ddffb
alexandre.sinicio <alexandre.sinicio@valentina>
2021-06-07 10:44:07 -0300

    #1

---
+++ app/entrypoint.php
@@ -0,0 +1,20 @@
+<?php
+use alesinicio\AsyncExecutor\AsyncExecutor;
+use alesinicio\AsyncExecutor\AsyncMultiProcess;
+use alesinicio\AsyncExecutor\AsyncProcess;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$cameras = file_get_contents(__DIR__.'/../config/cameras.json');
+$cameras = json_decode($cameras, true);
+
+$async = new AsyncExecutor('cvlc');
+$multiAsync	= new AsyncMultiProcess($async);
+
+$options = '--play-and-exit --sout="#transcode{vcodec=MJPG,venc=ffmpeg{strict=1},fps=10,width=640,height=360}:standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:${PORT}/}"';
+$i = 1;
+foreach($cameras as $camera) {
+	$option = str_replace('${PORT}', $camera['port'], $options);
+	$multiAsync->addProcess(new AsyncProcess('camera_'.$i, '', [$camera['url'], $option]));
+	$multiAsync->keepRunningProcesses();
+}
